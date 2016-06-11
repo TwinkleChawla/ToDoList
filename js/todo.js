f5c7fde9c30cde4d24ee214e77c566878aab9e93
Twinkle Chawla <twinkle1chawla@gmail.com>
2016-06-11 22:50:53 +0530

    indentation error resolved

--- js/todo.js
+++ js/todo.js
@@ -1,31 +1,29 @@
-    function get_todos() {
-        var todos = new Array;
-        var todos_str = localStorage.getItem('todo');
-        if (todos_str !== null) {
-            todos = JSON.parse(todos_str); 
-        }
-        return todos;
+function get_todos() {
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str); 
     }
+    return todos;
+}
      
-    function add() {
-        var task = document.getElementById('task').value;
-        var todos = get_todos();
-        todos.push(task);
-        localStorage.setItem('todo', JSON.stringify(todos));
-        show();
-        return false;
-    }
+function add() {
+    var task = document.getElementById('task').value;
+    var todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
+    show();
+    return false;
+}
      
-    function remove() {
-        var id = this.getAttribute('id');
-        var todos = get_todos();
-        todos.splice(id, 1);
-        localStorage.setItem('todo', JSON.stringify(todos));
-     
-        show();
-     
-        return false;
-    }
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = get_todos();
+    todos.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(todos)); 
+    show(); 
+    return false;
+}
 
     //Random Colour generator
     /*function getRandomColor() {
@@ -37,42 +35,40 @@
     return color;
     }*/
 
-    function show() {
-        var todos = get_todos();
-        var html = '<div class="col s6" style="margin-left:1rem;">';
-        for(var i=0; i<todos.length; i++) {
-            html += '<div class="card-panel col s6" class="drag" id="' + i  + '" draggable="true" ondragstart="drag(event)">' + todos[i]  + '<button class="remove" style="float:right" id="' + i  + '"></div>';
-        };
-        html += '</div>';
-     
-        document.getElementById('todos').innerHTML = html;
-     
-        var buttons = document.getElementsByClassName('remove');
-        for (var i=0; i < buttons.length; i++) {
-            buttons[i].addEventListener('click', remove);
-        };
-    }
+function show() {
+    var todos = get_todos();
+    var html = '<div class="col s6" style="margin-left:1rem;">';
+    for(var i=0; i<todos.length; i++) {
+        html += '<div class="card-panel col s6" class="drag" id="' + i  + '" draggable="true" ondragstart="drag(event)">' + todos[i]  + '<button class="remove" style="float:right" id="' + i  + '"></div>';
+    };
+    html += '</div>'; 
+    document.getElementById('todos').innerHTML = html; 
+    var buttons = document.getElementsByClassName('remove');
+    for (var i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+    };
+}
 
-    function drag(ev) {
-        ev.dataTransfer.setData("Text", ev.target.id); 
-        } 
+function drag(ev) {
+    ev.dataTransfer.setData("Text", ev.target.id); 
+} 
 
-    function allowDrop(ev) {
+function allowDrop(ev) {
     ev.preventDefault();
     if (ev.target.getAttribute("draggable") == "true")
         ev.dataTransfer.dropEffect = "none"; //drop blocked!!
     else
         ev.dataTransfer.dropEffect = "all";
-    }
+}
 
-    function drop(ev) {
+function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("Text");
     ev.target.appendChild(document.getElementById(data));
     localStorage.setItem('item', ev.target.id);
     var item = localStorage.getItem('item');
     return item;
-    }
+}
 
-   document.getElementById('add').addEventListener('click', add);
-    show(); 
+document.getElementById('add').addEventListener('click', add);
+show(); 
